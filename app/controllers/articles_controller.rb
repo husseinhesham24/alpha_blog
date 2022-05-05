d3e1548e7b5501bdf868089ec1ab62aaa844f2f1
husseinhesham24 <husseinhesham24@gmail.com>
2022-05-05 23:49:20 +0200

    re-factoring code and fix validation messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+	
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+	
 	def show
-		@article = Article.find(params[:id])
-		#debugger
 	end
 
 	def index
@@ -13,35 +14,41 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
-		# debugger
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was created successfully."
 			redirect_to @article 
 		else
-			render "new"
+			render "new", status: :unprocessable_entity
 		end
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params)
 			flash[:notice] = "Article was created successfully."
 			redirect_to @article 
 		else
-			render 'edit'
+			render 'edit', status: :unprocessable_entity
 		end
 	end
 
 	def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-		#debugger
     redirect_to articles_path
   end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
 end
