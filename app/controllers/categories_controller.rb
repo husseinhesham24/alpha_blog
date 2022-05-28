3bfa4095f4b7ac27a67ab376660ef3ffedf17474
husseinhesham24 <husseinhesham24@gmail.com>
2022-05-28 21:46:22 +0200

    adding category create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def new
-
+    @category = Category.new
   end
 
   def index
@@ -10,5 +10,23 @@ class CategoriesController < ApplicationController
 
   def show
 
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully."
+      redirect_to @category
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
